baa026a58196c0be0226223f07069187519c6ac6
jan4dev <jan4dev@gmail.com>
2015-08-08 10:52:41 +0200

    Added possibility with protractor to override from command line the browser to test.

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,13 +1,25 @@
+var _ = require('lodash');
 exports.config = {
     specs: [
         'e2e/**/*.js'
     ],
     rootElement: '.js-app',
     directConnect: true,
-    multiCapabilities:[{
-        browserName: 'chrome'
-    },{
-        browserName: 'firefox'
-    }],
+    getMultiCapabilities: function () {
+
+        var capabilities = _({
+            chrome: {browserName: 'chrome'},
+            firefox: {browserName: 'firefox'}
+        });
+
+        if (!this.params.browsers) {
+            return capabilities.values().value();
+        }
+        else {
+            var browsers = this.params.browsers.split(',');
+            // Using lodash to select the keys in `capabilities` corresponding to the browsers param.
+            return capabilities.pick(browsers).values().value();
+        }
+    },
     framework: 'jasmine2'
 };
